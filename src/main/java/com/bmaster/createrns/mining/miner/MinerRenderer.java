cd714769c0415808942a3447402eb50e0ea0419d
BMasta <bmaster635@gmail.com>
2025-08-31 15:45:35 -0700

    render miner with flywheel

--- src/main/java/com/bmaster/createrns/mining/miner/MinerRenderer.java
+++ src/main/java/com/bmaster/createrns/mining/miner/MinerRenderer.java
@@ -1,7 +1,10 @@
 package com.bmaster.createrns.mining.miner;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.simibubi.create.AllPartialModels;
+import com.simibubi.create.content.kinetics.base.SingleAxisRotatingVisual;
+import dev.engine_room.flywheel.api.visualization.VisualizationManager;
 import net.createmod.catnip.animation.AnimationTickHolder;
 import net.createmod.catnip.render.CachedBuffers;
 import net.minecraft.client.renderer.RenderType;
@@ -11,6 +14,7 @@ import net.createmod.catnip.render.SuperByteBuffer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class MinerRenderer extends KineticBlockEntityRenderer<MinerBlockEntity> {
     public MinerRenderer(BlockEntityRendererProvider.Context context) {
@@ -20,21 +24,15 @@ public class MinerRenderer extends KineticBlockEntityRenderer<MinerBlockEntity> 
     @Override
     protected void renderSafe(MinerBlockEntity be, float partialTicks, PoseStack ms, MultiBufferSource buffer,
                               int light, int overlay) {
-        final Axis axis = Axis.Y;
-        float time = AnimationTickHolder.getRenderTime(be.getLevel());
+        if (VisualizationManager.supportsVisualization(be.getLevel())) return;
 
+        VertexConsumer vb = buffer.getBuffer(RenderType.solid());
         SuperByteBuffer shaft = CachedBuffers.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(),
                 Direction.UP);
         SuperByteBuffer drill_head = CachedBuffers.partialFacing(AllPartialModels.DRILL_HEAD, be.getBlockState(),
                 Direction.DOWN);
 
-
-        float angle = (time * be.getSpeed() * 3f / 10) % 360; // Degrees
-        angle = angle / 180f * (float) Math.PI; // Radians
-
-        kineticRotationTransform(shaft, be, axis, angle, light);
-        kineticRotationTransform(drill_head, be, axis, angle, light);
-        shaft.renderInto(ms, buffer.getBuffer(RenderType.cutoutMipped()));
-        drill_head.renderInto(ms, buffer.getBuffer(RenderType.cutoutMipped()));
+        renderRotatingBuffer(be, shaft, ms, vb, light);
+        renderRotatingBuffer(be, drill_head, ms, vb, light);
     }
 }
