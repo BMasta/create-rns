8b4684a29e7ac85e948401ac7b0f2715e8f7efbb
BMasta <bmaster635@gmail.com>
2025-11-03 09:56:16 -0800

    add miner speed to spec, prioritize config value, change mining area schema in spec

--- src/main/java/com/bmaster/createrns/infrastructure/ServerConfig.java
+++ src/main/java/com/bmaster/createrns/infrastructure/ServerConfig.java
@@ -12,21 +12,27 @@ public class ServerConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
     // ------------------------------------------------ Config values ----------------------------------------------- //
-    private static final ForgeConfigSpec.DoubleValue MINER_MK1_SPEED_CV = BUILDER
-            .comment(" How many mining operations a miner mk1 can complete in one hour\n" +
-                    " at 256 RPM, with one deposit block claimed, and no deposit multipliers.")
-            .defineInRange("minerMk1Speed", 45.0, 0.0, Short.MAX_VALUE);
+    private static final ForgeConfigSpec.ConfigValue<Float> MINER_MK1_SPEED_CV = BUILDER
+            .comment("""
+                     How many mining operations a miner mk1 can complete in one hour
+                     at 256 RPM, with one deposit block claimed, and no deposit multipliers.
+                     Set to 0 to use the value defined in miner spec.\
+                    """)
+            .define("minerMk1Speed", 45f);
 
-    private static final ForgeConfigSpec.DoubleValue MINER_MK2_SPEED_CV = BUILDER
-            .comment(" How many mining operations a miner mk2 can complete in one hour\n" +
-                    " at 256 RPM, with one deposit block claimed, and no deposit multipliers.")
-            .defineInRange("minerMk2Speed", 45.0, 0.0, Short.MAX_VALUE);
+    private static final ForgeConfigSpec.ConfigValue<Float> MINER_MK2_SPEED_CV = BUILDER
+            .comment("""
+                     How many mining operations a miner mk2 can complete in one hour
+                     at 256 RPM, with one deposit block claimed, and no deposit multipliers
+                     Set to 0 to use the value defined in miner spec.\
+                    """)
+            .define("minerMk2Speed", 45f);
 
     public static final ForgeConfigSpec SPEC = BUILDER.build();
 
     // ------------------------------------------------ Baked values ------------------------------------------------ //
-    public static int minerMk1BaseProgress;
-    public static int minerMk2BaseProgress;
+    public static int minerMk1BaseProgress = 0;
+    public static int minerMk2BaseProgress = 0;
 
     // -------------------------------------------------------------------------------------------------------------- //
     @SubscribeEvent
@@ -35,7 +41,11 @@ public class ServerConfig {
         if (event.getConfig().getSpec() != ServerConfig.SPEC) return;
 
         var ticksPerHour = 60 * SharedConstants.TICKS_PER_MINUTE;
-        minerMk1BaseProgress = 256 * ticksPerHour / (int) MINER_MK1_SPEED_CV.get().floatValue();
-        minerMk2BaseProgress = 256 * ticksPerHour / (int) MINER_MK2_SPEED_CV.get().floatValue();
+        if (MINER_MK1_SPEED_CV.get() != 0) {
+            minerMk1BaseProgress = 256 * ticksPerHour / (int) MINER_MK1_SPEED_CV.get().floatValue();
+        }
+        if (MINER_MK2_SPEED_CV.get() != 0) {
+            minerMk2BaseProgress = 256 * ticksPerHour / (int) MINER_MK2_SPEED_CV.get().floatValue();
+        }
     }
 }
