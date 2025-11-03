24d37b5db7986967278be1cec37e919b283a6bc1
BMasta <bmaster635@gmail.com>
2025-11-03 09:56:16 -0800

    add miner speed to spec, prioritize config value, change mining area schema in spec

--- src/main/java/com/bmaster/createrns/infrastructure/ServerConfig.java
+++ src/main/java/com/bmaster/createrns/infrastructure/ServerConfig.java
@@ -13,20 +13,26 @@ public class ServerConfig {
 
     // ------------------------------------------------ Config values ----------------------------------------------- //
     private static final ModConfigSpec.ConfigValue<Float> MINER_MK1_SPEED_CV = BUILDER
-            .comment(" How many mining operations a miner mk1 can complete in one hour\n" +
-                    " at 256 RPM, with one deposit block claimed, and no deposit multipliers")
-            .define("minerSpeed", 45f);
+            .comment("""
+                     How many mining operations a miner mk1 can complete in one hour
+                     at 256 RPM, with one deposit block claimed, and no deposit multipliers.
+                     Set to 0 to use the value defined in miner spec.\
+                    """)
+            .define("minerMk1Speed", 45f);
 
     private static final ModConfigSpec.ConfigValue<Float> MINER_MK2_SPEED_CV = BUILDER
-            .comment(" How many mining operations a miner mk2 can complete in one hour\n" +
-                    " at 256 RPM, with one deposit block claimed, and no deposit multipliers")
-            .define("minerSpeed", 45f);
+            .comment("""
+                     How many mining operations a miner mk2 can complete in one hour
+                     at 256 RPM, with one deposit block claimed, and no deposit multipliers
+                     Set to 0 to use the value defined in miner spec.\
+                    """)
+            .define("minerMk2Speed", 45f);
 
     public static final ModConfigSpec SPEC = BUILDER.build();
 
     // ------------------------------------------------ Baked values ------------------------------------------------ //
-    public static int minerMk1BaseProgress;
-    public static int minerMk2BaseProgress;
+    public static int minerMk1BaseProgress = 0;
+    public static int minerMk2BaseProgress = 0;
 
     // -------------------------------------------------------------------------------------------------------------- //
     @SubscribeEvent
@@ -35,7 +41,11 @@ public class ServerConfig {
         if (event.getConfig().getSpec() != ServerConfig.SPEC) return;
 
         var ticksPerHour = 60 * SharedConstants.TICKS_PER_MINUTE;
-        minerMk1BaseProgress = 256 * ticksPerHour / (int) MINER_MK1_SPEED_CV.get().floatValue();
-        minerMk2BaseProgress = 256 * ticksPerHour / (int) MINER_MK2_SPEED_CV.get().floatValue();
+        if (MINER_MK1_SPEED_CV.get() != 0) {
+            minerMk1BaseProgress = 256 * ticksPerHour / (int) MINER_MK1_SPEED_CV.get().floatValue();
+        }
+        if (MINER_MK2_SPEED_CV.get() != 0) {
+            minerMk2BaseProgress = 256 * ticksPerHour / (int) MINER_MK2_SPEED_CV.get().floatValue();
+        }
     }
 }
--- src/main/java/com/bmaster/createrns/mining/miner/MinerSpec.java
+++ src/main/java/com/bmaster/createrns/mining/miner/MinerSpec.java
@@ -9,16 +9,22 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 
-public record MinerSpec(Block minerBlock, int tier, int miningAreaRadius, int miningAreaHeight,
-                        int miningAreaVerticalDisplacement) {
+public record MinerSpec(Block minerBlock, int tier, float minesPerHour, MiningArea miningArea) {
+    private static final Codec<MiningArea> maCodec = RecordCodecBuilder.create(i -> i.group(
+       Codec.intRange(0, Integer.MAX_VALUE).fieldOf("radius").forGetter(MiningArea::radius),
+       Codec.intRange(0, Integer.MAX_VALUE).fieldOf("height").forGetter(MiningArea::height),
+       Codec.INT.fieldOf("vertical_offset").forGetter(MiningArea::verticalOffset)
+    ).apply(i, MiningArea::new));
+
     public static final Codec<MinerSpec> CODEC = RecordCodecBuilder.create(i -> i.group(
             BuiltInRegistries.BLOCK.byNameCodec().fieldOf("miner_block").forGetter(MinerSpec::minerBlock),
             Codec.INT.fieldOf("tier").forGetter(MinerSpec::tier),
-            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("mining_area_radius").forGetter(MinerSpec::miningAreaRadius),
-            Codec.intRange(0, Integer.MAX_VALUE).fieldOf("mining_area_height").forGetter(MinerSpec::miningAreaHeight),
-            Codec.INT.fieldOf("mining_area_vertical_displacement").forGetter(MinerSpec::miningAreaVerticalDisplacement)
+            Codec.FLOAT.fieldOf("mines_per_hour").forGetter(MinerSpec::minesPerHour),
+            maCodec.fieldOf("mining_area").forGetter(MinerSpec::miningArea)
     ).apply(i, MinerSpec::new));
 
     public static final ResourceKey<Registry<MinerSpec>> REGISTRY_KEY =
             ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(CreateRNS.MOD_ID, "miner_spec"));
+
+    public record MiningArea(int radius, int height, int verticalOffset) {}
 }
