e2040f08d8bd78904e573e03a8b174bf4ae89689
BMasta <bmaster635@gmail.com>
2026-03-05 20:40:22 -0800

    add tooltip hints to miner components

--- src/main/java/com/bmaster/createrns/event/ClientEvents.java
+++ src/main/java/com/bmaster/createrns/event/ClientEvents.java
@@ -7,6 +7,9 @@ import com.bmaster.createrns.compat.ponder.RNSPonderPlugin;
 import com.bmaster.createrns.content.deposit.claiming.DepositClaimerOutlineRenderer;
 import com.bmaster.createrns.content.deposit.scanning.DepositScannerClientHandler;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -15,6 +18,7 @@ import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.InputEvent;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
+import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 
 @EventBusSubscriber(modid = CreateRNS.ID, value = Dist.CLIENT)
 public class ClientEvents {
@@ -62,5 +66,23 @@ public class ClientEvents {
         RNSParticleTypes.RESONANCE.register(RNSParticleTypes.RESONANCE_TYPE.get(), event);
         RNSParticleTypes.STABILIZING_RESONANCE.register(RNSParticleTypes.STABILIZING_RESONANCE_TYPE.get(), event);
         RNSParticleTypes.SHATTERING_RESONANCE.register(RNSParticleTypes.SHATTERING_RESONANCE_TYPE.get(), event);
+    }
+
+    @SubscribeEvent
+    public static void onItemTooltip(ItemTooltipEvent event) {
+        var stack = event.getItemStack();
+        var tooltip = event.getToolTip();
+        var itemId = BuiltInRegistries.ITEM.getKey(stack.getItem());
+        var alwaysTooltipKey = "item." + itemId.getNamespace() + "." + itemId.getPath() + ".tooltip.always";
+        var descriptionBasedKey = stack.getDescriptionId() + ".tooltip.always";
+        for (int i = 1;;++i) {
+            if (I18n.exists(alwaysTooltipKey + i)) {
+                tooltip.add(Component.translatable(alwaysTooltipKey + i));
+            } else if (I18n.exists(descriptionBasedKey + i)) {
+                tooltip.add(Component.translatable(descriptionBasedKey + i));
+            } else {
+                break;
+            }
+        }
     }
 }
