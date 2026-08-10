a335fd9fa696c484d08ebf1102d4f651ba5f53ef
BMasta <bmaster635@gmail.com>
2026-08-10 13:39:16 -0700

    fix issue where errors in unrelated recipes would show as mining recipe errors

--- src/main/java/com/bmaster/createrns/compat/kubejs/RNSKubeJSPlugin.java
+++ src/main/java/com/bmaster/createrns/compat/kubejs/RNSKubeJSPlugin.java
@@ -4,17 +4,11 @@ import com.bmaster.createrns.CreateRNS;
 import dev.latvian.mods.kubejs.KubeJSPlugin;
 import dev.latvian.mods.kubejs.client.LangEventJS;
 import dev.latvian.mods.kubejs.generator.DataJsonGenerator;
-import dev.latvian.mods.kubejs.recipe.RecipesEventJS;
 import dev.latvian.mods.kubejs.recipe.schema.RegisterRecipeSchemasEvent;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
-import dev.latvian.mods.kubejs.util.ConsoleJS;
 import net.minecraft.MethodsReturnNonnullByDefault;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeManager;
 
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.Map;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
@@ -43,17 +37,6 @@ public class RNSKubeJSPlugin extends KubeJSPlugin {
     @Override
     public void registerRecipeSchemas(RegisterRecipeSchemasEvent event) {
         event.register(CreateRNS.asResource("mining"), MiningRecipeKubeSchema.schema());
-    }
-
-    @Override
-    public void injectRuntimeRecipes(RecipesEventJS event, RecipeManager manager,
-                                     Map<ResourceLocation, Recipe<?>> recipesByName) {
-        int failures = event.failedCount.get();
-        if (failures == 0) return;
-
-        ConsoleJS.SERVER.error(failures + (failures == 1
-                ? " mining recipe failed to parse, check KubeJS warnings for details"
-                : " mining recipes failed to parse, check KubeJS warnings for details"));
     }
 
     @Override
